00642f86d5eae636c8f5bb40d867f387e21e3dab
James Gamboa <63767707+jayg555@users.noreply.github.com>
2021-02-20 19:55:10 -0800

    update

--- algorithms/duplicateZeros.js
+++ algorithms/duplicateZeros.js
@@ -17,16 +17,13 @@ Explanation: After calling your function, the input array is modified to: [1,2,3
 */ 
 
 function duplicateZeros(arr){
-	//let result = arr.length
 	for(let i=0; i<arr.length;i++){
 		if(arr[i] === 0){
 			arr.splice(i,0,0);
 			i++;
 			arr.pop();
 		}
-	}
-console.log(arr)
-	
+	}	
 }
 
 duplicateZeros([1,0,2,3,0,4,5,0])
